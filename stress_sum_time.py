7af79b9ddac5f487919706c5c28998f0cb1fa16f
jianhuizhai <jianhuizhai89@gmail.com>
2017-09-05 15:15:42 +0200

    Add files via upload

--- stress_sum_time.py
+++ stress_sum_time.py
@@ -1,46 +1,50 @@
 import os
 import shutil
 import numpy as np
-totalstep=200
+begin=200
+n=4
 step=200
+totalstep=begin+step*(n-1)
+
 title = 'id, x coord, y coord, z coord, sigma_xx, sigma_yy,sigma_zz,sigma_xy,sigma_xz,sigma_yz\n'
-list=[[]]*(totalstep/step+1)
-for i in range(0,totalstep+step,step):
+list=[[]]*(n)
+for i in range(begin,totalstep+step,step):
     name=str(i)
     csvfile=name.zfill(5)+'.csv'
     if os.path.exists(csvfile):
         os.remove(csvfile)
-for i in range(0,totalstep+step,step):
+for i in range(begin,totalstep+step,step):
     file='dump.HEA.equil.'+str(i)
     name=str(i)
     newfile=name.zfill(5)+'.csv'
     f=open(file)
     newfile=open(newfile,'a')
     newfile.write(title)
-    list[i/step]=[]
+    position=(i-begin)/step
+    list[position]=[]
     for line in f.readlines()[9:]:
         row=line.split()
         newfile.write((row[0]+','+row[1]+','+row[2]+','+row[3]+','
                    +row[4]+','+row[5]+','+row[6]+','+
                        row[7]+','+row[8]+','+row[9]+'\n'))
 
-        list[i/step].append(row[0]);list[i/step].append(row[1])
-        list[i/step].append(row[2]);list[i/step].append(row[3])
-        list[i/step].append(row[4]);list[i/step].append(row[5])
-        list[i/step].append(row[6]);list[i/step].append(row[7])
-        list[i/step].append(row[8]);list[i/step].append(row[9])
+        list[position].append(row[0]);list[position].append(row[1])
+        list[position].append(row[2]);list[position].append(row[3])
+        list[position].append(row[4]);list[position].append(row[5])
+        list[position].append(row[6]);list[position].append(row[7])
+        list[position].append(row[8]);list[position].append(row[9])
 
     f.close()
     newfile.close()
 
-summary=([[]]*(totalstep/step+1))
-for i in range(0,totalstep/step+1,1):
+summary=([[]]*n)
+for i in range(0,n,1):
     summary[i]=[]
     for j in range(0,len(list[0]),1):
         summary[i].append(float(list[i][j]))
 
 
-results=np.array(summary).sum(axis=0)/(totalstep/step+1)
+results=np.array(summary).sum(axis=0)/n
 
 
 sumfile='sum.csv'
